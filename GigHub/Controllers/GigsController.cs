d0db59e14f793157f7b0721e2984d2be6a9eead7
ethanchang1984 <zuomin.zhang@moodys.com>
2016-08-14 03:10:13 +0000

    update Gigs controllers

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -7,6 +7,7 @@ namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
