78ae564cb0964039477402313808da5a53e96548
ethanchang1984 <zuomin.zhang@moodys.com>
2016-09-16 02:01:49 +0000

    follow a artist

--- GigHub/Controllers/FollowingsController.cs
+++ GigHub/Controllers/FollowingsController.cs
@@ -1,4 +1,7 @@
-﻿using GigHub.Models;
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
 using System.Web.Http;
 
 namespace GigHub.Controllers
@@ -16,6 +19,19 @@ namespace GigHub.Controllers
         [HttpPost]
         public IHttpActionResult Follow(FollowingeDto dto)
         {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(a => a.FollowerId == userId && a.FolloweeId == dto.FolloweeId))
+                return BadRequest("The following already exists.");
+
+            var following = new Following()
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
             return Ok();
         }
     }
---
+++ GigHub/Dtos/FollowingeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GigHub.Dtos
+{
+    public class FollowingeDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
